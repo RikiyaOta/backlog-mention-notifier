e62da567cf62794d64461335d18f4cacc2fb72c1
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-05-03 16:56:46 +0900

    Add the API Gateway resource to our cdk stack.

--- lib/backlog-mention-notifier-stack.ts
+++ lib/backlog-mention-notifier-stack.ts
@@ -1,16 +1,48 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { RustFunction } from 'cargo-lambda-cdk';
+import { Stack, StackProps, aws_apigateway as apigw } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { RustFunction } from "cargo-lambda-cdk";
 
-export class BacklogMentionNotifierStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+const getEnv = () => process.env.NODE_ENV ?? "dev";
 
-    new RustFunction(this, 'backlog-mention-notifier', {
-      manifestPath: './Cargo.toml',
-      bundling: {
-        environment: {}
-      }
-    });
-  }
+export class BacklogMentionNotifierStack extends Stack {
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
+
+		const backlogMentionNotifierFunction = new RustFunction(
+			this,
+			`backlog-mention-notifier-${getEnv()}`,
+			{
+				manifestPath: "./Cargo.toml",
+				bundling: {
+					environment: {},
+				},
+			},
+		);
+
+		const nameRestApi = "Rest API with Lambda auth";
+		const restApi = new apigw.RestApi(this, nameRestApi, {
+			restApiName: `backlog-mention-notifier-${getEnv()}`,
+			deployOptions: {
+				stageName: getEnv(),
+				// TODO: Restrict source IP addresses.
+			},
+		});
+
+		const slackResource = restApi.root.addResource("slack");
+		slackResource.addMethod(
+			"POST",
+			new apigw.LambdaIntegration(backlogMentionNotifierFunction, {
+				// NOTE: Lambda を非同期で呼び出す.
+				proxy: false,
+				requestParameters: {
+					"integration.request.header.X-Amz-Invocation-Type": "'Event'",
+				},
+				integrationResponses: [{ statusCode: "200" }],
+			}),
+			{
+				// Lambdaを非同期で呼び出した場合のステータスコードは202になるので合わせる
+				methodResponses: [{ statusCode: "202" }],
+			},
+		);
+	}
 }
