d2d403b765e262e2ba3b3372c80bdc7a4f7780e7
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-05-03 16:23:48 +0900

    Add a rust function.

--- lib/backlog-mention-notifier-stack.ts
+++ lib/backlog-mention-notifier-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { RustFunction } from 'cargo-lambda-cdk';
 
 export class BacklogMentionNotifierStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BacklogMentionNotifierQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new RustFunction(this, 'backlog-mention-notifier', {
+      manifestPath: '../Cargo.toml',
+      bundling: {
+        environment: {}
+      }
+    });
   }
 }
