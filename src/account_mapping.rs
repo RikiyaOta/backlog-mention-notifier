d314f5a767d034afbabc90c32045cc99d91cbb83
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-05-05 15:31:11 +0900

    Resolve user accounts between Backlog and Slack.

--- src/account_mapping.rs
+++ src/account_mapping.rs
@@ -1,6 +1,6 @@
 use config::{Config, FileFormat};
-use serde::Deserialize;
 use core::panic;
+use serde::Deserialize;
 use std::env;
 use std::fs;
 
@@ -20,35 +20,64 @@ impl From<config::Config> for AppConfig {
     fn from(value: config::Config) -> Self {
         Self {
             slack_bot_oauth_token: value.get("slack_bot_oauth_token").unwrap(),
-            user_account_mapping: value.get("user_account_mapping").unwrap()
+            user_account_mapping: value.get("user_account_mapping").unwrap(),
         }
-    } 
+    }
 }
 
 pub fn get_app_config() -> AppConfig {
     let app_env = env::var("APP_ENV").unwrap_or_else(|_| String::from("development"));
+
     println!("app_env={}", app_env);
+
     let config_filename = format!("config/config.{}.json", app_env);
 
     let config_str = match fs::read_to_string(&config_filename) {
-       Ok(config) => config,
-       Err(error) => panic!("Cannot read the app configuration! error={:?}", error),
+        Ok(config) => config,
+        Err(error) => panic!("Cannot read the app configuration! error={:?}", error),
     };
 
     let settings = match Config::builder()
         .add_source(config::File::from_str(&config_str, FileFormat::Json))
-        .build() {
-            Ok(settings) => settings,
-            Err(error) => panic!("Cannot build the app configuration! error={:?}", error),
-        };
+        .build()
+    {
+        Ok(settings) => settings,
+        Err(error) => panic!("Cannot build the app configuration! error={:?}", error),
+    };
 
     let app_config = match TryInto::<AppConfig>::try_into(settings) {
         Ok(app_config) => app_config,
         Err(error) => panic!("Cannot convert to app config! error={:?}", error),
     };
 
-    println!("Slack bot OAuth token: {}", app_config.slack_bot_oauth_token);
-    println!("User account mapping: {:?}", app_config.user_account_mapping);
-    
+    println!(
+        "Slack bot OAuth token: {}",
+        app_config.slack_bot_oauth_token
+    );
+    println!(
+        "User account mapping: {:?}",
+        app_config.user_account_mapping
+    );
+
     app_config
+}
+
+pub fn get_backlog_users(app_config: &AppConfig) -> Vec<String> {
+    app_config
+        .user_account_mapping
+        .iter()
+        .map(|mapping| (&mapping.backlog_user_name).to_string())
+        .collect()
+}
+
+pub fn map_to_slack_user_id(app_config: &AppConfig, backlog_user_name: &String) -> Option<String> {
+    let user_account_mapping = &app_config.user_account_mapping;
+
+    match user_account_mapping
+        .iter()
+        .find(|mapping| &mapping.backlog_user_name == backlog_user_name)
+    {
+        Some(UserAccountMapping { slack_user_id, .. }) => Some(slack_user_id.to_string()),
+        None => None,
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,29 @@
 pub mod account_mapping;
+pub mod backlog_webhook_parser;
 
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, RequestPayloadExt, Response};
-use regex::Regex;
-use serde::{Deserialize, Serialize};
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 
-// https://crates.io/crates/lambda_runtime
-// https://github.com/cargo-lambda/cargo-lambda/issues/397
-
-#[derive(Deserialize, Serialize, Debug)]
-#[allow(non_snake_case)]
-struct BacklogProject {
-    projectKey: String,
-    name: String,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct BacklogUser {
-    name: String,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct BacklogNotification {
-    user: BacklogUser,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct BacklogComment {
-    id: u32,
-    content: String,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct BacklogCommentAddedContent {
-    key_id: u32,
-    summary: String,
-    comment: BacklogComment,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct BacklogIssueRelatedWebhookPayload {
-    id: u32,
-    project: BacklogProject,
-    r#type: u8,
-    content: BacklogCommentAddedContent,
-    notifications: Vec<BacklogNotification>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct CommentedIssue {
-    project_key: String,
-    issue_key: u32,
-    issue_subject: String,
-    // TODO: ↓何が扱いやすいかな。ID？
-    notified_backlog_users: Vec<String>,
-    comment: String,
-}
-
-fn is_commented_event(payload: &BacklogIssueRelatedWebhookPayload) -> bool {
-    payload.r#type == 3
-}
-
-/*
- * 注意：コメントの文章の中に、ただの文字としてメンションの情報が入っている。
- * また、ユーザー名の後に空白等が入っているとも限らない。"@RikiyaOtaホゲ" もありえる。
- * なので、あらかじめユーザー名を保持しておいて、その情報をもとに Regex を組み立ててサーチする必要がある。
- */
-fn extract_backlog_user_names(comment: &String) -> Vec<String> {
-    let backlog_user_names = vec!["RikiyaOta", "Test1", "Test2"];
-
-    backlog_user_names
-        .iter()
-        .filter(|backlog_user_name| {
-            let regex = Regex::new(&format!("@{}", backlog_user_name)).unwrap();
-            regex.is_match(&comment)
-        })
-        .map(|s| s.to_string())
-        .collect()
-}
-
-fn parse_webhook_payload(event: &Request) -> Result<CommentedIssue, String> {
-    match event.payload::<BacklogIssueRelatedWebhookPayload>() {
-        Ok(payload) => match payload {
-            None => Err("Payload is None.".to_string()),
-            Some(payload) => {
-                if is_commented_event(&payload) {
-                    let comment = payload.content.comment.content;
-                    Ok(CommentedIssue {
-                        project_key: payload.project.projectKey,
-                        issue_key: payload.content.key_id,
-                        issue_subject: payload.content.summary,
-                        // TODO: Parse notified users.
-                        notified_backlog_users: extract_backlog_user_names(&comment),
-                        comment,
-                    })
-                } else {
-                    Err("This payload is not commented-event.".to_string())
-                }
-            }
-        },
-        Err(error) => Err(error.to_string()),
-    }
-}
-
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // TODO:
-    // 1. Parse Request Body.
-    // 2. Judge webhook event type.
-    // 3. Get comment content.
-    // 4. Extract mentioned users.
-    // 5. Decide the users' slack account id.
-    // 6. Post DMs to the users.
-
-    match parse_webhook_payload(&event) {
-        Ok(payload) => println!("Payload: {:?}", payload),
-        Err(error_message) => println!("Error: {}", error_message),
-    }
-    
-    // DEBUG:
     let app_config = account_mapping::get_app_config();
-    println!("App Config: {:?}", app_config);
+
+    match backlog_webhook_parser::parse_webhook_payload(&event, &app_config) {
+        Err(error_message) => println!("Error: {}", error_message),
+        Ok(commented_issue) => {
+            commented_issue
+                .notified_backlog_user_names
+                .iter()
+                .for_each(|backlog_user_name| {
+                    let maybe_slack_user_id =
+                        account_mapping::map_to_slack_user_id(&app_config, &backlog_user_name);
+                    println!(
+                        "backlog_user_name: {}, slack_user_id: {:?}",
+                        backlog_user_name, maybe_slack_user_id
+                    );
+
+                    // 後はこの　Slack User ID と Bot OAuth Token を使ってメッセージを投げるだけ！
+                });
+        }
+    }
 
     // Extract some useful information from the request
     let who = event
